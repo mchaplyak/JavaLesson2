e0b42ede673058d06737991023aace253908653c
chaplyak <m.chapliak@gmail.com>
2020-11-29 13:58:51 +0200

    New Class - Veterinary

---
+++ src/Veterinary.java
@@ -0,0 +1,12 @@
+public class Veterinary {
+
+    public Veterinary() {
+    }
+
+    public void treatAnimal(String food, String location) {
+        System.out.println(
+                        " Регулярне харчування: " + food +
+                        "\n Звідки: " + location
+        );
+    }
+}

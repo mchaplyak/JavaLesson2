a73f03feab531fea8002e7724c654a45b3b69289
chaplyak <m.chapliak@gmail.com>
2020-11-29 13:59:05 +0200

    Ended homework

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        Animal[] animals = new Animal[3];
+        animals[0] = new Cat("Корм", "Львів", false, true);
+        animals[1] = new Dog("Корм", "Харків", false, true);
+        animals[2] = new Horse("Сіно", "Київ", false, true);
+        int n = 1;
+
+        Veterinary veterinarySergeyPetrovich = new Veterinary();
+
+        for (Animal animal : animals) {
+            System.out.println("Тваринка №" + n);
+            veterinarySergeyPetrovich.treatAnimal(animal.food, animal.location);
+            n++;
+        }
 
     }
+
+
+
 }

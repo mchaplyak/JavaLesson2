23e4c2853a0aa7c7a149419b26e062573e32baae
chaplyak <m.chapliak@gmail.com>
2020-11-29 13:25:07 +0200

    Class dog ended

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,27 @@
 public class Dog extends Animal {
 
+    boolean makeWafWaf;
 
+
+    public Dog(boolean food, String location, boolean makeWafWaf) {
+        super(food, location);
+        this.makeWafWaf = makeWafWaf;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Пес гавкає");
+    }
+
+    @Override
+    public void eat(boolean food) {
+        this.food = food;
+        if (this.food) {
+            System.out.println("Пес їсть");
+        } else {
+            System.out.println("Пес не голодний");
+        }
+    }
 
 
 

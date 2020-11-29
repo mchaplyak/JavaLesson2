ccb67986ecbe38453947c8b375a6f96525ad5b4b
chaplyak <m.chapliak@gmail.com>
2020-11-29 12:53:16 +0200

    Create Classes - Main, Animal, Cat, Dog, Horse

---
+++ src/Animal.java
@@ -0,0 +1,45 @@
+public class Animal {
+    private String food;
+    private String location;
+
+    public Animal() {
+    }
+
+    public Animal(String food, String location) {
+        this.food = food;
+        this.location = location;
+    }
+
+    public void makeNoise(String animalName) {
+        System.out.println(animalName + " sleep");
+    }
+
+    public void eat() {
+    }
+    public void sleep() {
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "food='" + food + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}

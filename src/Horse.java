990c0f2ab120396b740aceb7a79b99b7ce0406f9
chaplyak <m.chapliak@gmail.com>
2020-11-29 13:26:48 +0200

    Class Horse ended

--- src/Horse.java
+++ src/Horse.java
@@ -1,2 +1,26 @@
 public class Horse extends Animal {
+
+    boolean makePrrPrr;
+
+
+    public Horse(boolean food, String location, boolean makePrrPrr) {
+        super(food, location);
+        this.makePrrPrr = makePrrPrr;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Кінь видає дивні звуки які не знаю як описати");
+    }
+
+    @Override
+    public void eat(boolean food) {
+        this.food = food;
+        if (this.food) {
+            System.out.println("Кінь їсть");
+        } else {
+            System.out.println("Кінь не голодний");
+        }
+    }
+
 }
